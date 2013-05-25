a141d5227b169b5f86d5f6552e53522bcb02eba4
Janne <Jane@janecomputer>
2013-05-25 19:43:26 +0200

    Add dynamical view for index/edit view of alias widget

--- controllers/widget.php
+++ controllers/widget.php
@@ -6,17 +6,10 @@ class Widget extends Admin_Controller
 	{
 		parent::__construct();
                 
-                $this->output->set_header("test", "test");
-		//$this->auth->restrict('Bonfire.Users.View');
-		//$this->load->model('file_manager_files_model');
-		//$this->load->model('file_manager_alias_model');
-		//$this->lang->load('users');
-		//Template::set_block('sub_nav', 'settings/_sub_nav');
 	}
 
-        public function alias($caller_module=null)
+        public function alias($caller_module=null, $table_row_id=null)
         {
-
 		// think about how to adjust automatically to if it should get files for the whole module or for a single row in the table
 		// what about multiple tables per module?
 
@@ -30,28 +23,27 @@ class Widget extends Admin_Controller
                 if(($caller_module)) $this->load->config($caller_module . '/config');
 
                 $module_name = null; // error message
-                if($module_config = $this->config->item('module_config')) if(isset($module_config['name'])) $module_name = $module_config['name']; 
+                if($module_config = $this->config->item('module_config')) if(isset($module_config['name'])) $module_name = $module_config['name'];
 
                 // NOT WORKING! since template class is not used
                 //if(is_null($module_name)) Template::set_message('"(An error occured while retrieving module configuration)"', 'error');
 
-//                $alias_records = $this->file_manager_alias_model->where('target_module', $caller_module)->find_all();
-                
-                // make this according to BF_model
-                $mysql_resource = mysql_query("
-                    SELECT f.`id`, f.`file_name`, f.`description`, f.`tags`, a.`target_table_row_id` FROM `ci_bf_git`.`".$this->db->dbprefix."file_manager_files` f, `ci_bf_git`.`".$this->db->dbprefix."file_manager_alias` a
-                    WHERE f.`id` = a.`file_id` AND a.`target_module` = '".$caller_module."'");
-
+                $is_table_row = !is_null($table_row_id) ? true : false;
                 $alias_records = null;
-                while($data = mysql_fetch_array($mysql_resource, MYSQL_ASSOC)) $alias_records[] = (object) $data;
 
+                // make this according to BF_model
+                $sql = "SELECT f.`id`, f.`file_name`, f.`description`, f.`tags`, a.`target_table_row_id` FROM `ci_bf_git`.`".$this->db->dbprefix."file_manager_files` f, `ci_bf_git`.`".$this->db->dbprefix."file_manager_alias` a
+                        WHERE f.`id` = a.`file_id` AND a.`target_module` = '".$caller_module."'";
+                if($is_table_row) $sql .= " AND a.`target_table_row_id` = '".$table_row_id."'";
+                $mysql_resource = mysql_query($sql);
+                while($data = mysql_fetch_array($mysql_resource, MYSQL_ASSOC)) $alias_records[] = (object) $data;
                 
                 $this->load->view('file_manager/widget/alias', array(
-                        'test' => null,
-                        'test2' => null,
-                        'test3' => null,
 			'alias_records' => $alias_records,
-                        'module_name' => $module_name));
+                        'is_table_row'  => $is_table_row,
+                        'table_row_id'  => $table_row_id,
+                        'module_name'   => $module_name
+                ));
         }
         
         public function download()
--- views/widget/alias.php
+++ views/widget/alias.php
@@ -1,18 +1,20 @@
 <div class="admin-box">
-        <h3>Available files for <?php echo $module_name; ?></h3>
+        <h3>Available files for <?php echo $module_name; ?><?php echo ($is_table_row) ? ', table row id: ' . $table_row_id : ''; ?></h3>
 	
 	<table class="table table-striped">
                     <thead>
 				<tr>
-					<?php if ($this->auth->has_permission('File_Manager_test.Content.Delete') && isset($alias_records) && is_array($alias_records) && count($alias_records)) : ?>
+                                <?php if ($this->auth->has_permission('File_Manager_test.Content.Delete') && isset($alias_records) && is_array($alias_records) && count($alias_records)) : ?>
 					<th class="column-check"><input class="check-all" type="checkbox" /></th>
-					<?php endif;?>
+                                <?php endif; ?>
 					
 					<th>ID</th>
 					<th>File name</th>
 					<th>Description</th>
 					<th>Tags</th>
-					<th>Related to table row</th>
+                                <?php if(!$is_table_row) : ?>
+                                        <th>Related to table row</th>
+                                <?php endif; ?>
 				</tr>
 			</thead>
 			<?php if (isset($alias_records) && is_array($alias_records) && count($alias_records)) : ?>
@@ -30,25 +32,27 @@
 			<?php endif; ?>
 			<tbody>
 			<?php if (isset($alias_records) && is_array($alias_records) && count($alias_records)) : ?>
-			<?php foreach ($alias_records as $alias_record) : ?>
-				<tr>
-					<?php if ($this->auth->has_permission('File_Manager_test.Content.Delete')) : ?>
+                                <?php foreach ($alias_records as $alias_record) : ?>
+                                        <tr>
+                                        <?php if ($this->auth->has_permission('File_Manager_test.Content.Delete')) : ?>
                                                 <td><input type="checkbox" name="checked[]" value="<?php echo $alias_record->id ?>" /></td>
-					<?php endif;?>
-					
-					<td><?php echo $alias_record->id; ?></td>
-        				<?php if ($this->auth->has_permission('file_manager.Content.Download')) : ?>
-						<td>
-                                                    <a target="_blank" href="<?php echo site_url(SITE_AREA .'/widget/file_manager/download/' . $alias_record->id); ?>"><?php echo $alias_record->file_name; ?></td>
-					<?php else: ?>
-                				<td><?php echo $alias_record->file_name; ?></td>
-					<?php endif; ?>
+                                        <?php endif;?>
 
-					<td><?php echo $alias_record->description; ?></td>
-					<td><?php echo $alias_record->tags; ?></td>
-					<td><?php echo $alias_record->target_table_row_id; ?></td>
-				</tr>
-			<?php endforeach; ?>
+                                                <td><?php echo $alias_record->id; ?></td>
+                                        <?php if ($this->auth->has_permission('file_manager.Content.Download')) : ?>
+                                                <td><a target="_blank" href="<?php echo site_url(SITE_AREA .'/widget/file_manager/download/' . $alias_record->id); ?>"><?php echo $alias_record->file_name; ?></td>
+                                        <?php else: ?>
+                                                <td><?php echo $alias_record->file_name; ?></td>
+                                        <?php endif; ?>
+
+                                                <td><?php echo $alias_record->description; ?></td>
+                                                <td><?php echo $alias_record->tags; ?></td>
+                                        <?php if(!$is_table_row) : ?>
+                                                <td><?php echo $alias_record->target_table_row_id; ?></td>
+                                        <?php endif; ?>
+
+                                        </tr>
+                                <?php endforeach; ?>
 			<?php else: ?>
 				<tr>
 					<td colspan="3">No records found that match your selection.</td>
@@ -59,12 +63,3 @@
 	
 	
 </div>
-
-<!--
-<pre><p>Test variables from <i>Modules::run('file_manager/widget/alias', $data);</i></p>-->
-	<?php/*
-	if(isset($test)) var_dump($test);
-	if(isset($test2)) var_dump($test2);
-	if(isset($test3)) var_dump($test3);
-	*/?>
-<!--</pre>-->
