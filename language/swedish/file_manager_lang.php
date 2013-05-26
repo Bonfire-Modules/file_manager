4cc1b60fb0d4bec902ab4cf7744196786f73d8a8
inbe <inbe@inventiveit.se>
2013-05-26 19:05:16 +0200

    Added language skeleton for swedish language.

---
+++ language/swedish/file_manager_lang.php
@@ -0,0 +1,55 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['file_manager_manage']			= 'Manage File Manager';
+$lang['file_manager_edit']				= 'Edit';
+$lang['file_manager_true']				= 'True';
+$lang['file_manager_false']				= 'False';
+$lang['file_manager_create']			= 'Create';
+$lang['file_manager_list']				= 'List';
+$lang['file_manager_new']				= 'New';
+$lang['file_manager_upload']				= 'Upload file';
+$lang['file_manager_edit_text']			= 'Edit this to suit your needs';
+$lang['file_manager_no_records']			= 'There aren\'t any file_manager in the system.';
+$lang['file_manager_create_new']			= 'Create a new File Manager.';
+$lang['file_manager_create_success']			= 'File Manager successfully created.';
+$lang['file_manager_create_failure']			= 'There was a problem creating the file_manager: ';
+$lang['file_manager_create_new_button']			= 'Create New File Manager';
+$lang['file_manager_invalid_id']			= 'Invalid File Manager ID.';
+$lang['file_manager_edit_uploading_success']			= 'The information was added to the uploaded file successfully.';
+$lang['file_manager_edit_success']			= 'File Manager successfully saved.';
+$lang['file_manager_edit_failure']			= 'There was a problem saving the file_manager: ';
+$lang['file_manager_delete_success']			= 'record(s) successfully deleted.';
+$lang['file_manager_delete_failure']			= 'We could not delete the record: ';
+$lang['file_manager_delete_error']			= 'You have not selected any records to delete.';
+$lang['file_manager_actions']			= 'Actions';
+$lang['file_manager_cancel']			= 'Cancel';
+
+$lang['file_manager_file_delete_record']		= 'Delete this file';
+$lang['file_manager_file_delete_confirm']		= 'Are you sure you want to delete this file?';
+
+$lang['file_manager_delete_record']			= 'Delete this File Manager';
+$lang['file_manager_delete_confirm']			= 'Are you sure you want to delete this file_manager?';
+$lang['file_manager_edit_heading']			= 'Edit File Manager';
+
+$lang['file_manager_yes']                               = 'Yes';
+$lang['file_manager_no']                                = 'No';
+
+$lang['file_manager_add_upload_information']            = 'Add upload information';
+
+$lang['file_manager_display_values_file_type']          = 'Type';
+$lang['file_manager_display_values_client_name']        = 'Original name';
+$lang['file_manager_display_values_file_ext']           = 'Extension';
+$lang['file_manager_display_values_file_size']          = 'Size';
+$lang['file_manager_display_values_image_width']        = 'Image width';
+$lang['file_manager_display_values_image_height']       = 'Image height';
+$lang['file_manager_display_values_database_row_id']    = 'Database id';
+$lang['file_manager_display_values_file_exists']        = 'File exists?';
+
+$lang['file_manager_toolbar_upload_failed']             = 'Upload failed';
+$lang['file_manager_message_upload_successful']         = 'File uploaded successfully';
+$lang['file_manager_message_file_exists']               = 'File selected for upload already exists';
+
+
+// Activities
+$lang['file_manager_act_create_record']			= 'Created record with ID';
+$lang['file_manager_act_edit_record']			= 'Updated record with ID';
+$lang['file_manager_act_delete_record']			= 'Deleted record with ID';
