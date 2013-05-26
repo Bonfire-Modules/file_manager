588ef0a0814c05072aa0b64e74eac8df5d0fb0ee
inbe <inbe@inventiveit.se>
2013-05-26 20:34:02 +0200

    Added migration/permissions for Content.Upload Changed _sub_nav to comply with migration/permissions

--- views/content/_sub_nav.php
+++ views/content/_sub_nav.php
@@ -3,7 +3,7 @@
 		<a href="<?php echo site_url(SITE_AREA .'/content/file_manager') ?>" id="list"><?php echo lang('file_manager_list'); ?></a>
 	</li>
 
-        <?php if ($this->auth->has_permission('file_manager.Content.Upload')) : ?>
+        <?php if ($this->auth->has_permission('File_manager.Content.Upload')) : ?>
                 <li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?> >
                         <a href="<?php echo site_url(SITE_AREA .'/content/file_manager/create') ?>" id="create_new"><?php echo lang('file_manager_upload'); ?></a>
                 </li>
