671724c3aa1d1033266abaafcea06cdcc95fec6c
Janne <Jane@janecomputer>
2013-05-26 15:20:08 +0200

    Modified view to handle content types and correct the attachment_name

--- views/widget/download.php
+++ views/widget/download.php
@@ -1,6 +1,6 @@
 <?php
-header("Content-Type: application/pdf");
-header("Content-Disposition: attachment; filename=\"" .$file_path. "\";");
+header("Content-Type: " . $content_type);
+header("Content-Disposition: attachment; filename=\"" . $attachment_name . "\";");
 header("Pragma: public");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 readfile($file_path);
