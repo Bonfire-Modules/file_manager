8b17ba18fc36ae044b020030d4bed3eca6dc7d82
Janne <Jane@janecomputer>
2013-06-04 00:54:51 +0200

    Added fields to alias migration

--- migrations/002_Install_file_manager.php
+++ migrations/002_Install_file_manager.php
@@ -67,6 +67,17 @@ class Migration_Install_file_manager extends Migration {
 				'type' => 'VARCHAR',
 				'constraint' => 255,
 			),
+			'override_description' => array(
+				'type' => 'TEXT'
+			),
+			'override_tags' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 255,
+			),
+			'override_public' => array(
+				'type' => 'TINYINT',
+				'constraint' => 1,
+			),
 			'target_module' => array(
 				'type' => 'INT',
 				'constraint' => 11,
