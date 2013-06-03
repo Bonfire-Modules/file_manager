de2ab5b10fa4355d5b33c6a60fe3ed000551da84
Janne <Jane@janecomputer>
2013-06-03 23:43:45 +0200

    Modified and added list links to _sub_nav

--- views/content/_sub_nav.php
+++ views/content/_sub_nav.php
@@ -1,6 +1,10 @@
 <ul class="nav nav-pills">
         <li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/file_manager') ?>" id="list"><?php echo lang('file_manager_list'); ?></a>
+		<a href="<?php echo site_url(SITE_AREA .'/content/file_manager') ?>" id="list"><?php echo lang('file_manager_list_files'); ?></a>
+	</li>
+
+        <li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
+		<a href="<?php echo site_url(SITE_AREA .'/content/file_manager') ?>" id="list"><?php echo lang('file_manager_list_alias'); ?></a>
 	</li>
 
         <?php if ($this->auth->has_permission('File_manager.Content.Upload')) : ?>
