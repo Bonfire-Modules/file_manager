a91d156b74f31115dd712db0d4ad12584d6ec28e
Janne <Jane@janecomputer>
2013-07-06 23:33:35 +0200

    incorrect message on last commit, is same as this: Added option to not delete files with aliases from index and edit view, includes template message in edit view

--- views/content/index.php
+++ views/content/index.php
@@ -11,5 +11,10 @@ if ($error_messages !== false) : ?>
 	<div class="admin-box">
 	<h3><?php echo lang('file_manager_file_browser'); ?></h3>
 
-	<?php echo Modules::run('datatable/widget/display', $datatableData, $datatableOptions, true, true, true, 'file_manager_files_model', 'callback_unlink_files', 'Content', false, 'id', 'ID'); ?>
+	<?php echo Modules::run('datatable/widget/display', $datatableData, $datatableOptions, true, true, true, 'file_manager_files_model', 'callback_unlink_files', 'Content', false, 'id', 'ID', false); ?>
+
+	<select name="delete_has_aliases" id="delete_has_aliases" style="position: relative; top: -36px; left: 180px;">
+		<option value="1">Don't delete files with aliases</option>
+		<option value="0">Delete files and aliases</option>
+	</select>
 </div>
