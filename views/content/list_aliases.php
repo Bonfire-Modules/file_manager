c5be2ded9f2a362e96df9d44a89bb82b00e08e1f
Janne <Jane@janecomputer>
2014-07-07 23:43:42 +0200

    remove all

--- views/content/list_aliases.php
+++
@@ -1,5 +0,0 @@
-<div class="admin-box">
-	<h3>Complete alias list</h3>
-
-        <?php echo Modules::run('datatable/widget/display', $datatableData, $datatableOptions, true, true, true, 'file_manager_alias_model', null, null, false, 'id', null); ?>
-</div>
--- views/content/view_image.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($error)) die($error);
-header("Content-Type: " . $content_type);
-header("Pragma: public");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-readfile($file_path);
-?>
